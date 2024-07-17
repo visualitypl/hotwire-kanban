dfadb63323490ff3a5235c40fe7481f90a49ad6b
Piotr Witek <p.witek@visuality.pl>
2024-07-17 17:50:10 +0200

    Implement real-time updates for board columns and cards

--- app/models/board_column.rb
+++ app/models/board_column.rb
@@ -1,6 +1,17 @@
 class BoardColumn < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :board
   has_many :cards, dependent: :nullify
 
   validates :name, presence: true
+
+  after_commit :broadcast_column
+
+  def broadcast_column
+    broadcast_replace_to(dom_id(board),
+                         target: dom_id(self),
+                         partial: "board_columns/board_column",
+                         locals: { board_column: self })
+  end
 end
