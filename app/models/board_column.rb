8055c3a0336e185566e2dd2e6a72456c6d42ba4d
Piotr Witek <p.witek@visuality.pl>
2024-07-18 12:30:58 +0200

    Add real-time updates for board views

--- app/models/board_column.rb
+++ app/models/board_column.rb
@@ -1,6 +1,10 @@
 class BoardColumn < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :board
   has_many :cards, dependent: :nullify
 
   validates :name, presence: true
+
+  broadcasts_to ->(board_column) { "board_#{board_column.board_id}" }, inserts_by: :append, target: [ :board, :columns ]
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -6,4 +6,17 @@ class Card < ApplicationRecord
   acts_as_list scope: :board_column
 
   delegate :board, to: :board_column
+
+  after_commit :touch_affected_board_columns
+
+  private
+
+  def touch_affected_board_columns
+    if previous_changes[:board_column_id].present?
+      board.board_columns.find_by(id: previous_changes[:board_column_id]&.first)&.touch
+      board.board_columns.find_by(id: previous_changes[:board_column_id]&.last)&.touch
+    else
+      board_column.touch
+    end
+  end
 end
