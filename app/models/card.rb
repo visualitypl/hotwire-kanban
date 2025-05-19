befe166399224c2d5ad6bf484b2c634ed4e993b8
Piotr Witek <pio.witek@gmail.com>
2025-05-19 11:52:35 +0200

    Broadcast removal of desctructed card

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,10 +10,15 @@ class Card < ApplicationRecord
   delegate :board, to: :board_column
 
   after_create_commit :broadcast_card_created
+  after_destroy_commit :broadcast_card_destroyed
 
   private
 
   def broadcast_card_created
     broadcast_append_to board, target: dom_id(board_column, :column_body), partial: "cards/card", locals: { card: self }
+  end
+
+  def broadcast_card_destroyed
+    broadcast_remove_to board, target: dom_id(self)
   end
 end
