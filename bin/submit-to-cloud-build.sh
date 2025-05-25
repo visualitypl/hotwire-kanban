42a290f7efd05ac873c04b5753c2dc1e79512cdf
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-05-25 15:31:46 +0200

    1.2 building manually as we speak

---
+++ bin/submit-to-cloud-build.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+. .env
+
+set -euo pipefail
+
+gcloud config set project $PROJECT_ID
+
+set -x
+
+gcloud builds submit --config cloudbuild.yaml \
+    --substitutions _SERVICE_NAME=$SERVICE_NAME,_INSTANCE_NAME=$INSTANCE_NAME,_REGION=$REGION,_SECRET_NAME=$RAILS_SECRET_NAME
--- iac/03-secret-manager.sh
+++ iac/03-secret-manager.sh
@@ -6,4 +6,21 @@ set -euo pipefail
 
 gcloud config set project $PROJECT_ID
 
-bin/rails credentials:edit
+echo DONE: bin/rails credentials:edit
+
+
+echo "[public]  RAILS_SECRET_NAME: $RAILS_SECRET_NAME"
+echo "[private]  RAILS_MASTER_KEY: $RAILS_MASTER_KEY"
+
+gcloud secrets create "$RAILS_SECRET_NAME" --data-file config/master.key
+gcloud secrets describe "$RAILS_SECRET_NAME" # --format="value(name)"
+
+#gcloud projects describe $PROJECT_ID --format='value(projectNumber)'
+
+gcloud secrets add-iam-policy-binding $RAILS_SECRET_NAME \
+    --member serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
+    --role roles/secretmanager.secretAccessor
+
+gcloud secrets add-iam-policy-binding $RAILS_SECRET_NAME \
+    --member serviceAccount::${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+    --role roles/secretmanager.secretAccessor
