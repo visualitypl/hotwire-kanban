9ab0224486944d4e80c9b1acae44e0dd4cb841f1
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-05-25 15:43:55 +0200

    1.2 this should fix ENV RAILS_KEY

--- bin/submit-to-cloud-build.sh
+++ bin/submit-to-cloud-build.sh
@@ -9,5 +9,5 @@ gcloud config set project $PROJECT_ID
 
 set -x
 
-gcloud builds submit --config cloudbuild.yaml \
+time gcloud builds submit --config cloudbuild.yaml \
     --substitutions _SERVICE_NAME=$SERVICE_NAME,_INSTANCE_NAME=$INSTANCE_NAME,_REGION=$REGION,_SECRET_NAME=$RAILS_SECRET_NAME
